f9b0807a76c1d5d0f1d3d59f7348f5ae66a58d15
RajMalHotRA123 <RM_frankfurt@gmx.de>
2021-04-18 13:54:35 +0200

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Raj Malhotra on 18.04.21.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
